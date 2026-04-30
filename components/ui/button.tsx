8cb68aa8420693466918660a8089ab2d806756af
aymenghris <aymen.quantum@gmail.com>
2026-04-30 10:24:28 +0100

    feat(ui): add `sidebar-action` variant to button component for improved group hover interaction

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -54,6 +54,12 @@ const buttonVariants = cva(
                     "text-muted-foreground text-sm",
                     "hover:bg-primary/5",
                 ),
+                "sidebar-action": cn(
+                    "ml-auto h-full",
+                    "rounded-sm opacity-0",
+                    "hover:bg-neutral-300 group-hover:opacity-100",
+                    "dark:hover:bg-neutral-600",
+                ),
             },
             size: {
                 default: "h-9 px-4 py-2 has-[>svg]:px-3",
