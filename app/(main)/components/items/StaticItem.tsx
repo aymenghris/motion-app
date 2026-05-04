2bf1d2c8184c55de2a4941018397fbf3eda4bdb9
aymenghris <aymen.quantum@gmail.com>
2026-05-04 10:17:13 +0100

    refactor(ui): enhance `StaticItem` with `forwardRef` and extended props to allow passing an item as child without neglecting other Radix UI props

--- app/(main)/components/items/StaticItem.tsx
+++ app/(main)/components/items/StaticItem.tsx
@@ -1,36 +1,49 @@
 import type { LucideIcon } from "lucide-react"
-import type { ReactNode } from "react"
+import { type ButtonHTMLAttributes, forwardRef } from "react"
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
 
-interface ItemProps {
+interface StaticItemProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     label: string
     icon: LucideIcon
-    onClick: () => void
-    children?: ReactNode
     active?: boolean
 }
 
-export const StaticItem = ({
-    label,
-    onClick,
-    icon: Icon,
-    children,
-    active = false,
-}: ItemProps) => {
-    return (
-        <Button
-            onClick={onClick}
-            variant="sidebar"
-            size="full"
-            className={cn("group pl-3", active && "bg-primary/5 text-primary")}
-            capitalized={false}
-        >
-            <Icon className="mr-2 h-4.5 shrink-0 text-muted-foreground" />
+export const StaticItem = forwardRef<HTMLButtonElement, StaticItemProps>(
+    (
+        {
+            label,
+            onClick,
+            icon: Icon,
+            children,
+            active = false,
+            className,
+            ...props
+        },
+        ref,
+    ) => {
+        return (
+            <Button
+                ref={ref}
+                onClick={onClick}
+                variant="sidebar"
+                size="full"
+                className={cn(
+                    "group pl-3",
+                    active && "bg-primary/5 text-primary",
+                    className,
+                )}
+                capitalized={false}
+                {...props}
+            >
+                <Icon className="mr-2 h-4.5 shrink-0 text-muted-foreground" />
 
-            <span className="truncate first-letter:uppercase">{label}</span>
+                <span className="truncate first-letter:uppercase">{label}</span>
 
-            {children}
-        </Button>
-    )
-}
+                {children}
+            </Button>
+        )
+    },
+)
+
+StaticItem.displayName = "StaticItem"
