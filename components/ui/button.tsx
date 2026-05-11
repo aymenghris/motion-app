7a6d92f9e9ed22041d88189b00077a767e10a6cb
aymenghris <aymen.quantum@gmail.com>
2026-05-11 15:46:11 +0100

    feat(ui): add `banner-action` and `sm-banner` button variants
    - Introduce `banner-action` variant with white font, border, and hover effects.
    - Add `sm-banner` size for compact banner buttons.

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils"
 const buttonVariants = cva(
     cn(
         "inline-flex shrink-0 items-center justify-center gap-2 whitespace-nowrap",
-        "cursor-pointer rounded-md font-medium text-sm",
+        "cursor-pointer rounded-sm font-medium text-sm",
         "outline-none transition-all",
         "focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50",
         "disabled:pointer-events-none disabled:opacity-50",
@@ -60,12 +60,19 @@ const buttonVariants = cva(
                     "hover:bg-neutral-300 group-hover:opacity-100",
                     "dark:hover:bg-neutral-600",
                 ),
-                "trash-action": "rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600",
+                "trash-action":
+                    "rounded-sm p-2 hover:bg-neutral-200 dark:hover:bg-neutral-600",
+                "banner-action": cn(
+                    "font-normal text-white",
+                    "border-1 border-white",
+                    "hover:bg-primary/5",
+                ),
             },
             size: {
                 default: "h-9 px-4 py-2 has-[>svg]:px-3",
                 xs: "h-6 gap-1 rounded-md px-2 text-xs has-[>svg]:px-1.5 [&_svg:not([class*='size-'])]:size-3",
                 sm: "h-8 gap-1.5 rounded-md px-3 has-[>svg]:px-2.5",
+                "sm-banner": "h-auto p-1 px-2",
                 full: "min-h-6.75 w-full py-1 pr-3",
                 lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
                 icon: "size-9",
