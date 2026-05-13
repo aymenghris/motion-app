c9fe128cae8a7157e17ca90229e090ac9a4df83e
aymenghris <aymen.quantum@gmail.com>
2026-05-13 15:34:37 +0100

    feat(documents): fetch and display document data in `DocumentPage` with toolbar integration

--- app/(main)/documents/[documentId]/page.tsx
+++ app/(main)/documents/[documentId]/page.tsx
@@ -1,5 +1,34 @@
+"use client"
+
+import { useQuery } from "convex/react"
+import { useParams } from "next/navigation"
+import { Toolbar } from "@/components/toolbar"
+import { api } from "@/convex/_generated/api"
+import type { DocumentId } from "@/types/documents"
+
 const DocumentPage = () => {
-    return <div>DocumentPage</div>
+    const params = useParams()
+    const documentId = params.documentId as DocumentId
+
+    const document = useQuery(api.documents.getDocumentById, {
+        id: documentId,
+    })
+
+    if (document === undefined) {
+        return <div>Loading...</div>
+    }
+
+    if (document === null) {
+        return <div>Document not found</div>
+    }
+
+    return (
+        <div className="pt-40">
+            <div className="mx-auto md:max-w-3xl lg:max-w-4xl">
+                <Toolbar initialData={document} />
+            </div>
+        </div>
+    )
 }
 
 export default DocumentPage
