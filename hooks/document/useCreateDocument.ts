5e27d7e7375fb75725cf1fb15df03ee5cf337e36
aymenghris <aymen.quantum@gmail.com>
2026-05-20 14:58:43 +0100

    feat(documents): redirect to document after creation

--- hooks/document/useCreateDocument.ts
+++ hooks/document/useCreateDocument.ts
@@ -1,13 +1,17 @@
 import { useMutation } from "convex/react"
+import { useRouter } from "next/navigation"
 import { toast } from "sonner"
 import { api } from "@/convex/_generated/api"
 
 export const useCreateDocument = () => {
+    const router = useRouter()
     const createDocument = useMutation(api.documents.createDocument)
 
     const handleCreateDocument = () => {
         const promise = createDocument({
             title: "Untitled",
+        }).then((documentId) => {
+            router.push(`/documents/${documentId}`)
         })
 
         toast.promise(promise, {
